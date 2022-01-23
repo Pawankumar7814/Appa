c40e8c3dbccfba0ca61acc982cc30c836ca41712
ShrishtiSi <Shrishtisinghal50@gmail.com>
2022-01-23 18:49:14 +0530

    updated

--- routes/emailroutes.js
+++ routes/emailroutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var router = express.Router();
 let config = require("../config/config.json");
-let transporter = require("../config/mailler");
+let transporter = require("../config/mailer");
 let ejs = require("ejs");
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
@@ -13,16 +13,16 @@ router.post("/sendemail", (req, res) => {
         UNAME: req.body.UNAME,
         Uemail: req.body.Uemail,
         Uphone: req.body.Uphone,
-        Umsg: req.body.Umsg
+        Umsg: req.body.Umsg,
     };
-    ejs.renderFile("views/email/thanksforcontact.ejs", { udata: data }, async(err, tfile) => {
+    ejs.renderFile("views/email/thanks.ejs", { udata: data }, async(err, tfile) => {
         if (err) {
             console.log(err);
         } else {
             console.log(tfile);
             let info = await transporter.sendMail({
-                from: config.uemail, // sender address
-                to: data.uemail, // list of receivers
+                from: config.email, // sender address
+                to: data.Uemail, // list of receivers
                 subject: "Hello ✔", // Subject line
                 text: "Hello world?", // plain text body
                 html: tfile, // html body
@@ -33,7 +33,7 @@ router.post("/sendemail", (req, res) => {
 });
 
 router.get("/test", (req, res) => {
-    res.render("../views/email/thanksforcontact.ejs");
+    res.render("../views/email/thanks.ejs");
 });
 
 module.exports = router;
