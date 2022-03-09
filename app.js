db8d44a3ed096aedb3a1af1a088cc97e04b60b07
shekharkundra <shekhar.kundra@gmail.com>
2022-03-09 19:53:14 +0530

    https done

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-// All require modules
-var express = require("express");
-var http = require("http");
-var ejs = require("ejs");
-var flash = require("connect-flash");
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var favicon = require("./config/favicon");
-
-
-// Creating appp
-var app = express();
-
-// Set view engine
-app.set("view engine", "ejs");
-
-// assign port number
-var port = 3100 | process.env.port;
-
-//token for jwt
-process.env.TOKEN_SECRET = require("crypto").randomBytes(64).toString('hex');
-
-//seting favicon by passing app
-favicon(app);
-
-//conected to flash message
-app.use(flash());
+{
+    "name": "appa",
+    "version": "1.0.0",
+    "description": "",
+    "main": "app.js",
+    "scripts": {
+        "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "author": "Pawan Kumar",
+    "license": "ISC",
+    "dependencies": {
+        "connect-flash": "^0.1.1",
+        "cookie-parser": "^1.4.6",
+        "ejs": "^3.1.6",
+        "express": "^4.17.2",
+        "express-session": "^1.17.2",
+        "flash": "^1.1.0",
+        "fs": "0.0.1-security",
+        "https": "^1.0.0",
+        "jsonwebtoken": "^8.5.1",
+        "mongoose": "^6.1.7",
+        "multer": "^1.4.4",
+        "nodemailer": "^6.7.2",
+        "serve-favicon": "^2.5.0",
+        "uuid": "^8.3.2"
+    }
+}
+pp.use(flash());
 app.use(cookieParser("this is cokkie for appa"));
 app.use(session({
     cookie: { maxAge: 60000 },
@@ -88,6 +89,6 @@ app.get("/*", (req, res) => {
 });
 
 // Creating server
-http.createServer(app).listen(port, () => {
+https.createServer(httpsOptions, app).listen(port, () => {
     console.log("port number = " + port);
 });
