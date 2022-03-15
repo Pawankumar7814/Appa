a511e3d87ab21c4d5c2ebc5419ac2af286f8882c
shekhar.kundra <shekhar.kundra@gmail.com>
2022-03-15 21:36:28 +0530

    og tag

--- routes/WebSiteRoutes/productroutes.js
+++ routes/WebSiteRoutes/productroutes.js
@@ -3,6 +3,7 @@ var express = require("express");
 var router = express.Router();
 var Product = require("../../controllers/Admin/ProductController");
 var product = new Product();
+var OgData = require("../../config/Og.json");
 
 // Route to index page
 router.get(["/products/", "/products/index"], (req, res) => {
@@ -19,12 +20,14 @@ router.get(["/products/", "/products/index"], (req, res) => {
 // Route for one product
 router.get(["/product/:id"], (req, res) => {
     console.log(req.params.id);
+
     product.getProductById(req.params.id, (CbData) => {
         if (CbData.status == "err") {
             console.log(err);
             return res.status(404).redirect("/error404");
         } else {
-            res.status(200).render("../views/WebSite/products/product.ejs", { title: "Wick 1 - Appa", data: CbData.data });
+            OgData.title = CbData.data.Title;
+            res.status(200).render("../views/WebSite/products/product.ejs", { title: "Wick 1 - Appa", data: CbData.data, Og: OgData });
         }
     });
 });
