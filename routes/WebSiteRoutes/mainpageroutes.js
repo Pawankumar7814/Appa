426594d9d20a8a68a47f078c240ba19a5708006a
shekhar.kundra <shekhar.kundra@gmail.com>
2022-03-16 21:04:03 +0530

    some update

--- routes/WebSiteRoutes/mainpageroutes.js
+++ routes/WebSiteRoutes/mainpageroutes.js
@@ -1,6 +1,8 @@
 // All require modules
 var express = require("express");
 var router = express.Router();
+var Product = require("../../controllers/Admin/ProductController");
+var product = new Product();
 var OgData = require("../../config/Og.json");
 
 // Route to index page
@@ -8,8 +10,13 @@ router.get(["/", "/index"], (req, res) => {
     OgData.title = "Home Page.";
     OgData.description = "Home Page All new products are shown on this page.";
     OgData.image = "/Images/ganesha-left.jpeg";
-    console.log(OgData);
-    res.status(200).render("../views/WebSite/mainpages/index.ejs", { title: "Home - Appa", Og: OgData });
+    product.getAllProducts((CbData) => {
+        if (CbData.status == "err") {
+            return res.status(404).redirect("/error404");
+        } else {
+            res.status(200).render("../views/WebSite/mainpages/index.ejs", { title: "Products - Appa", data: CbData.data, Og: OgData });
+        }
+    });
 });
 
 router.get(["/indexsh"], (req, res) => {
